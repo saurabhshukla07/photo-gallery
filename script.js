f3d85eb4a53435c128dcea0eea85ae0bb7fb7610
Saurabh <sidshukla8416@gmail.com>
2024-04-29 08:06:04 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+var typed = new Typed(".text",{
+    strings:["so beautiful", "so elegent", "just looking like wow 😍"],
+    typeSpeed : 10,
+    backSpeed : 100,
+    backDelay : 1000,
+    loop : true
+});
+
